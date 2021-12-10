8654673746664388f7bd5055c5d8991063bf92c1
hismichael <skyiah@qq.com>
2021-12-10 17:48:37 +0800

    system.management

---
+++ Source/DeviceInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace SkyTerm
+{
+    struct ComPort // custom struct with our desired values
+    {
+        public string name;
+        public string vid;
+        public string pid;
+        public string description;
+    }
+
+    public class DeviceInfo
+    {
+        private const string vidPattern = @"VID_([0-9A-F]{4})";
+        private const string pidPattern = @"PID_([0-9A-F]{4})";
+
+        private List<ComPort> GetSerialPorts()
+        {
+            using (var searcher = new ManagementObjectSearcher
+                ("SELECT * FROM WIN32_SerialPort"))
+            {
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                return ports.Select(p =>
+                {
+                    ComPort c = new ComPort();
+                    c.name = p.GetPropertyValue("DeviceID").ToString();
+                    c.vid = p.GetPropertyValue("PNPDeviceID").ToString();
+                    c.description = p.GetPropertyValue("Caption").ToString();
+
+                    Match mVID = Regex.Match(c.vid, vidPattern, RegexOptions.IgnoreCase);
+                    Match mPID = Regex.Match(c.vid, pidPattern, RegexOptions.IgnoreCase);
+
+                    if (mVID.Success)
+                        c.vid = mVID.Groups[1].Value;
+                    if (mPID.Success)
+                        c.pid = mPID.Groups[1].Value;
+
+                    return c;
+                }).ToList();
+            }
+        }
+    }
+}
