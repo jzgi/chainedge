1f75deeff02957aa9c4d710d807333e6d13b131e
hismichael <skyiah@qq.com>
2023-12-07 16:09:51 +0800

    webrtc

--- Source/Drivers/ObjectDetectorDriver.cs
+++ Source/Drivers/ObjectDetectorDriver.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using ChainFx;
-using FlashCap;
 using Image = System.Drawing.Image;
 
 namespace ChainEdge.Drivers
@@ -31,21 +30,21 @@ namespace ChainEdge.Drivers
 
         public double Loss(float output, float label)
         {
-            var devices = new CaptureDevices();
-
-            foreach (var descriptor in devices.EnumerateDescriptors())
-            {
-                // "Logicool Webcam C930e: DirectShow device, Characteristics=34"
-                // "Default: VideoForWindows default, Characteristics=1"
-                Console.WriteLine(descriptor);
-
-                foreach (var characteristics in descriptor.Characteristics)
-                {
-                    // "1920x1080 [JPEG, 30fps]"
-                    // "640x480 [YUYV, 60fps]"
-                    Console.WriteLine(characteristics);
-                }
-            }
+            // var devices = new CaptureDevices();
+            //
+            // foreach (var descriptor in devices.EnumerateDescriptors())
+            // {
+            //     // "Logicool Webcam C930e: DirectShow device, Characteristics=34"
+            //     // "Default: VideoForWindows default, Characteristics=1"
+            //     Console.WriteLine(descriptor);
+            //
+            //     foreach (var characteristics in descriptor.Characteristics)
+            //     {
+            //         // "1920x1080 [JPEG, 30fps]"
+            //         // "640x480 [YUYV, 60fps]"
+            //         Console.WriteLine(characteristics);
+            //     }
+            // }
             return 0;
         }
 
@@ -61,29 +60,29 @@ namespace ChainEdge.Drivers
 
         public async Task ByScan()
         {
-            var devices = new CaptureDevices();
-
-// Open a device with a video characteristics:
-            var descriptor0 = devices.EnumerateDescriptors().ElementAt(0);
-
-            await using var device = await descriptor0.OpenAsync(
-                descriptor0.Characteristics[0], bufferScope =>
-                {
-                    // Captured into a pixel buffer from an argument.
-
-                    // Get image data (Maybe DIB/JPEG/PNG):
-                    byte[] image = bufferScope.Buffer.ExtractImage();
-
-                    // Anything use of it...
-                    var ms = new MemoryStream(image);
-                    var bitmap = Bitmap.FromStream(ms);
-                    return Task.CompletedTask;
-
-                    // ...
-                });
+//             var devices = new CaptureDevices();
+//
+// // Open a device with a video characteristics:
+//             var descriptor0 = devices.EnumerateDescriptors().ElementAt(0);
+//
+//             await using var device = await descriptor0.OpenAsync(
+//                 descriptor0.Characteristics[0], bufferScope =>
+//                 {
+//                     // Captured into a pixel buffer from an argument.
+//
+//                     // Get image data (Maybe DIB/JPEG/PNG):
+//                     byte[] image = bufferScope.Buffer.ExtractImage();
+//
+//                     // Anything use of it...
+//                     var ms = new MemoryStream(image);
+//                     var bitmap = Bitmap.FromStream(ms);
+//                     return Task.CompletedTask;
+//
+//                     // ...
+//                 });
 
 // Start processing:
-            await device.StartAsync();
+            // await device.StartAsync();
         }
 
         public int GetNumberByScan()
